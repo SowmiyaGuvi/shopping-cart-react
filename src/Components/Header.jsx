61cd09bc522457a823e91d1e8dbcb93308e4b7b7
SowmiyaGuvi <sowmimiya43@gmail.com>
2024-11-09 21:56:33 +0530

    added shopping cart

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return  <header className="bg-dark py-5" >
+  <div className="container px-4 px-lg-5 my-5">
+      <div className="text-center text-white">
+          <h1 className="display-4 fw-bolder">Celebrate Your Uniqueness</h1>
+          <p className="lead fw-normal text-white-50 mb-0">A change you can see. A few clicks is all it takes. A fresh approach to shopping. A great place to be. A higher form of shopping.</p>
+      </div>
+  </div>
+</header>
+}
+
+export default Header
